b0afc1efa069adfd6456dee95d0bb90610deaa1e
Taylor Musolf <tcmusolf@gmail.com>
2021-02-16 17:32:25 -0800

    Finish select_within_select, aggregates, joins_warmup

--- w5d2/skeleton 2/lib/04_select_within_select.rb
+++ w5d2/skeleton 2/lib/04_select_within_select.rb
@@ -128,6 +128,16 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
-    
+    SELECT
+      name, continent, population
+    FROM
+      countries
+    WHERE NOT continent IN (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE population > 25000000
+    )
   SQL
 end
