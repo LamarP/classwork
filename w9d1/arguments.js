9ea11d548486e0ea6970251ebf2b082655d628f7
lpoole <keithpoole123@gmail.com>
2021-03-15 16:26:08 -0400

    sum

---
+++ w9d1/arguments.js
@@ -0,0 +1,23 @@
+//Sum
+
+// function sum() {
+//   let result = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     result += arguments[i];
+//   }
+//   return result;
+// }
+
+// function sum(...args) {
+//   let sum = 0;
+//   for (let i = 0; i < args.length; i++) {
+//     sum += args[i];
+//   }
+//   return sum;
+// }
+
+
+// console.log(sum(1, 2, 3, 4)); //=== 10;
+// console.log(sum(1, 2, 3, 4, 5)); //=== 15;
+
+
