3efe75b734fda8fd00e7e9cd66aa0004f27027b2
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-24 15:37:47 -0700

    Complete CSS

--- w10d3_pair/frontend/components/board.jsx
+++ w10d3_pair/frontend/components/board.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import Tile from "./tile";
+
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
-    return <h1>board</h1>
+    const board = this.props.board;
+    return (
+      <div>
+        {board.grid.map((row, i) =>{
+          return (
+          <div key={i}
+            className="row">
+            {row.map((tile, j) => {
+              return (<Tile
+                key={j}
+                tile={tile}
+                updateGame={this.props.updateGame}/>)
+            })}
+          </div>
+          )
+        })}
+      </div>
+    )
   };
 }
 export default Board;
--- w10d3_pair/frontend/components/tile.jsx
+++ w10d3_pair/frontend/components/tile.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { Tile } from "../../minesweeper";
+
 
 class Tile extends React.Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
   }
 
   render() {
-    return <p>T</p>
+    const tile = this.props.tile;
+    let text = "";
+
+    if (tile.explored){
+      if (tile.bombed){
+        text = "BOOM!";
+      } else {
+        let num = tile.adjacentBombCount();
+        if (num > 0){
+          text = `${num}`;
+        } else {
+          text = "";
+        };
+      }
+    } else if (tile.flagged){
+      text = "Flagged!";
+    }
+    return (
+      <div>
+        <a>{text}</a>
+      </div>
+    )
   }
 }
 export default Tile;
